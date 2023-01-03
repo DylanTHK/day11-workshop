9b71a7a56d522a5f77e5618a2cd6bc17a74c6f1a
DylanTHK <dylantanhk@gmail.com>
2023-01-03 13:49:34 +0800

    added spring project

---
+++ src/main/java/com/workshop/day11/Day11Application.java
@@ -0,0 +1,13 @@
+package com.workshop.day11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11Application.class, args);
+	}
+
+}
